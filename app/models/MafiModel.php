b7ab120aab9e98992ce64be13287405cb1b5f3ef
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 15:26:41 -0500

    Metodo replicar datosafi,Consulta log debug

--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -29,7 +29,7 @@ class MafiModel{
             if(!$consulta):
                 return False;
             else:
-                return $consulta;
+                //return $consulta;
             endif;
             //return $consulta;
         /*} catch (PDOException $e) {

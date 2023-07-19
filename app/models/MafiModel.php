a364d403eae4f99b9ac03a7080acdfd29d14513a
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 14:15:49 -0500

    Metodo replicar datosafi, insercion datosMafiReplica, logAplicacion y indice_de_cambios, debug

--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -32,8 +32,7 @@ class MafiModel{
     }
 
     public function insertEstudiante($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir){
-        var_dump($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir);die();
-        try {
+        //try {
             $insertEstudiante= $this->db->connect()->prepare("INSERT INTO `datosMafiReplica` SET 
                                                                           `idbanner` = ?, 
                                                                           `primer_apellido` = ?, 
@@ -62,10 +61,11 @@ class MafiModel{
             $insertEstudiante->bindValue(11,$operador,PDO::PARAM_STR);
             $insertEstudiante->bindValue(12,$autorizadoAsistir,PDO::PARAM_STR);
             $insertEstudiante->execute();
-            return $insertEstudiante;
+            var_dump($insertEstudiante);die();
+            /*return $insertEstudiante;
         } catch (PDOException $e) {
             return false;
-        }
+        }*/
     }
 
     public function insertLog($primerId,$ultimoRegistroId,$fechaInicio,$fechaFin,$accion,$tablaAfectada,$mensajeLog){

54eec9110e85609c26db9572eaf51d1101759808
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 09:05:37 -0500

    prueba url

--- app/libs/app.php
+++ app/libs/app.php
@@ -9,7 +9,7 @@ class App{
     {
         date_default_timezone_set("America/Bogota");
         $url = $this->separarURL();
-        var_dump($url[0]);die();
+        var_dump(ucwords($url[0]));die();
         if($url != '' && file_exists("../app/controllers/".ucwords($url[0]).".php")):
             $this->controller = ucwords($url[0]);
             unset($url[0]);

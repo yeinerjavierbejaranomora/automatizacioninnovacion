6d7829851f832d5b563d9cdff61e91d8607c2d52
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 10:13:52 -0500

    Database Coneccion

--- app/libs/database.php
+++ app/libs/database.php
@@ -3,10 +3,10 @@ class Database{
     private $host,$db,$user,$password,$charset,$pdo;
     function __construct()
     {
-        $this->host = 'localhost';
+        $this->host = '127.0.0.1';
         $this->db = 'u266816196_ILPoF';
-        $this->user = 'root';
-        $this->password = '';
+        $this->user = 'u266816196_YCF0b';
+        $this->password = 'Yeiner91041755542.';
         $this->charset = 'utf8mb4';
     }
 

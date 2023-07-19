67ba26aa43a633b5e636db1867e093f31b36ea73
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 15:52:28 -0500

    .htaccess public

--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -23,7 +23,7 @@ class MafiModel{
 
     function dataMafi($offset){
         try {
-            $consulta = $this->db->connect()->prepare("SELECT * FROM `datosMafi` WHERE `id` > ? AND `estado` = 'Activo' AND `sello` IN ('TIENE RETENCION', 'TIENE SELLO FINANCIERO') ORDER BY `id` ASC");
+            $consulta = $this->db->connect()->prepare("SELECT * FROM `datosMafi` WHERE `id` > 2 AND `estado` = 'Activo' AND `sello` IN ('TIENE RETENCION', 'TIENE SELLO FINANCIERO') ORDER BY `id` ASC");
             $consulta->bindValue(1,$offset,PDO::PARAM_INT);
             $consulta->execute();
             return $consulta;

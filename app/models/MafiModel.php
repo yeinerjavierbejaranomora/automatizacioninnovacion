295f60069d89aca79775ad8d9329bb3aad75dc25
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 15:19:18 -0500

    Metodo replicar datosafi,Consulta log debug

--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -23,7 +23,7 @@ class MafiModel{
 
     function dataMafi($offset){
         // var_dump($offset);die();
-        //try {
+        try {
             $consulta = $this->db->connect()->prepare("SELECT * FROM `datosMafi` WHERE `id` > ? AND `estado` = 'Activo' AND `sello` IN ('TIENE RETENCION', 'TIENE SELLO FINANCIERO') ORDER BY `id` ASC");
             $consulta->bindValue(1,$offset,PDO::PARAM_INT);
             $consulta->execute();
@@ -32,10 +32,10 @@ class MafiModel{
             else:
                 return $consulta;
             endif;
-            /*return $consulta;
+            //return $consulta;
         } catch (PDOException $e) {
             return false;
-        }*/
+        }
     }
 
     public function insertEstudiante($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir){

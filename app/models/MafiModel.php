bd789ef5ca93281d0f10a006ff21affbf700596a
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-19 11:25:29 -0500

    Metodo replicar datosafi,Consulta log

--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -10,16 +10,15 @@ class MafiModel{
     }
 
     public function log($accion,$tabla){
-        //try {
+        try {
             $consultaLog = $this->db->connect()->prepare("SELECT * FROM `logAplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
             $consultaLog->bindValue(1,$accion,PDO::PARAM_STR);
             $consultaLog->bindValue(2,$tabla,PDO::PARAM_STR);
             $consultaLog->execute();
-            var_dump($consultaLog);die();
-            /*return $consultaLog;
+            return $consultaLog;
         } catch (PDOException $e) {
             return false;
-        }*/
+        }
     }
 
     function dataMafi(){

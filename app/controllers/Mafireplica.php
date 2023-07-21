3acf6ca7d78830dce6191a6ad4ad761de33d53a1
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-21 16:21:08 -0500

    [replicar datosMafiReplica en estudiantes. y alertas tempranas ]

--- app/controllers/Mafireplica.php
+++ app/controllers/Mafireplica.php
@@ -15,7 +15,7 @@ class Mafireplica  extends Controller{
         else:
             $offset = 0;
         endif;
-        $limit = 15000;
+        $limit = 4000;
         $datosNum = $this->model->numeroDatosMafi($offset);
         $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);
         if ($datosNumFetch['totalEstudiantes'] > 0) :

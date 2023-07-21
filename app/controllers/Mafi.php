33a4e30ad1fbc2b360e2f263cc33aab3e78373c6
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-21 08:54:22 -0500

    [replicar datosMafi en datosMafiReplica]

--- app/controllers/Mafi.php
+++ app/controllers/Mafi.php
@@ -20,6 +20,7 @@ class Mafi extends Controller{
         $datosNum = $this->model->numeroDatosMafi($offset);
         $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);
         $datosMafi = $this->model->dataMafi2($offset);
+        var_dump($datosMafi);die();
         if($datosNumFetch['total'] > 0):
             $numeroRegistros = 0;
             $primerId = $this->model->dataMafi()->fetch(PDO::FETCH_ASSOC)['id'];

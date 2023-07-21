9149523d6c94249d701e05d397533f31cf7ddf26
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-21 12:06:50 -0500

    [replicar datosMafiReplica en estudiantes. y alertas tempranas ][historial consulta]

--- app/controllers/Mafireplica.php
+++ app/controllers/Mafireplica.php
@@ -36,7 +36,7 @@ class Mafireplica  extends Controller{
                 $programaAbrio = NULL;
                 if(str_contains($tipoEstudiante,'TRANSFERENTE')):
                     $historial = $this->model->historialEstudiante($codigoBanner);
-                    var_dump($estudiante['id'],$codigoBanner,$programa,$tipoEstudiante,"TRANSFERENTE");die();
+                    var_dump($historial->fetch(PDO::FETCH_ASSOC));die();
                 else:
                     //var_dump($codigoBanner,$programa,$tipoEstudiante,"NO TRANSFERENTE");die();
                 endif;

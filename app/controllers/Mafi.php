607fe9cf38990ce17e278a86d7e5d1e4b597bde7
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-21 09:00:18 -0500

    [replicar datosMafi en datosMafiReplica]

--- app/controllers/Mafi.php
+++ app/controllers/Mafi.php
@@ -17,16 +17,16 @@ class Mafi extends Controller{
         else:
             $offset = 0;
         endif;
-        $datosNum = $this->model->numeroDatosMafi($offset);
-        $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);
-        if($datosNumFetch['total'] > 0):
-            $datosMafi = $this->model->dataMafi($offset);
-            var_dump($datosMafi);die();
+        //var_dump($offset);die();
+        $datosMafi = $this->model->dataMafi($offset);
+        //if($datosMafi && $this->model->dataMafi()->fetch(PDO::FETCH_ASSOC)['id'] > $offset):
+        if($datosMafi):
             $numeroRegistros = 0;
-            $primerId = $this->model->dataMafi()->fetch(PDO::FETCH_ASSOC)['id'];
+            //$primerId = $this->model->dataMafi()->fetch(PDO::FETCH_ASSOC)['id'];
+            $primerId = 0;
             $ultimoRegistroId = 0;
             $fechaInicio = date('Y-m-d H:i:s');
-            foreach ($datosMafi as $estudiante) :
+            foreach($datosMafi as $estudiante):
                 $idBanner = $estudiante['idbanner'];
                 $primerApellido = $estudiante['primer_apellido'];
                 $programa = $estudiante['programa'];
@@ -39,24 +39,24 @@ class Mafi extends Controller{
                 $sello = $estudiante['sello'];
                 $operador = $estudiante['operador'];
                 $autorizadoAsistir = $estudiante['autorizado_asistir'];
-                if ($sello == 'TIENE RETENCION' && ($autorizadoAsistir == 'ACTIVO EN PLATAFORMA' || $autorizadoAsistir == 'ACTIVO EN PLATAFORMA ICETEX')) :
-                    $insertEstudiante = $this->model->insertEstudiante($idBanner, $primerApellido, $programa, $codPrograma, $cadena, $periodo, $estado, $tipoEstudiante, $rutaAcademica, $sello, $operador, $autorizadoAsistir);
+                if($sello == 'TIENE RETENCION' && ($autorizadoAsistir == 'ACTIVO EN PLATAFORMA' || $autorizadoAsistir == 'ACTIVO EN PLATAFORMA ICETEX')):
+                    $insertEstudiante = $this->model->insertEstudiante($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir);
                     $numeroRegistros++;
-                elseif ($sello == 'TIENE SELLO FINANCIERO') :
-                    $insertEstudiante = $this->model->insertEstudiante($idBanner, $primerApellido, $programa, $codPrograma, $cadena, $periodo, $estado, $tipoEstudiante, $rutaAcademica, $sello, $operador, $autorizadoAsistir);
+                elseif($sello == 'TIENE SELLO FINANCIERO'):
+                    $insertEstudiante = $this->model->insertEstudiante($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir);
                     $numeroRegistros++;
                 endif;
                 $ultimoRegistroId = $estudiante['id'];
                 $idBannerUltimoRegistro = $idBanner;
             endforeach;
             $fechaFin = date('Y-m-d H:i:s');
-            $accion = 'Insert';
+            $accion ='Insert';
             $tablaAfectada = 'datosMafiReplica';
             $mensajeLog = 'Se realizo la insercion en la tabla datosMafiRelica desde la tabla datosMafi, iniciando en el id ' . $primerId . ' y terminando en el id ' . $ultimoRegistroId . ',insertando ' . $numeroRegistros . ' registros';
-            $insertarLog = $this->model->insertLog($primerId, $ultimoRegistroId, $fechaInicio, $fechaFin, $accion, $tablaAfectada, $mensajeLog);
-            $insertIndice = $this->model->insertIndice($idBannerUltimoRegistro, $accion, $mensajeLog);
+            $insertarLog = $this->model->insertLog($primerId,$ultimoRegistroId,$fechaInicio,$fechaFin,$accion,$tablaAfectada,$mensajeLog); 
+            $insertIndice = $this->model->insertIndice($idBannerUltimoRegistro,$accion,$mensajeLog);
             if ($insertarLog && $insertIndice) :
-                echo "Fecha inicio: " . $fechaInicio . ', Fecha Fin ' . $fechaFin;
+                echo "Fecha inicio: " .$fechaInicio. ', Fecha Fin '. $fechaFin;
                 echo "Numero de registros: '$numeroRegistros'=> primer id registrado: " . $primerId . ', Ultimo id registrado ' . $ultimoRegistroId;
             endif;
         else:
--- app/models/MafiModel.php
+++ app/models/MafiModel.php
@@ -34,13 +34,17 @@ class MafiModel{
     }
 
     function dataMafi($offset){
-            $consulta = $this->db->connect()->prepare("SELECT * FROM `datosMafi` WHERE `id` > ? AND  `estado` = 'Activo' AND `sello` IN ('TIENE RETENCION', 'TIENE SELLO FINANCIERO') ORDER BY `id` ASC");
-            $consulta->bindValue(1,$offset,PDO::PARAM_INT);
+        //var_dump($offset);die();
+        try {
+            $consulta = $this->db->connect()->prepare("SELECT * FROM `datosMafi` WHERE `id` > 0 AND  `estado` = 'Activo' AND `sello` IN ('TIENE RETENCION', 'TIENE SELLO FINANCIERO') ORDER BY `id` ASC");
+            //$consulta->bindValue(1,$offset,PDO::PARAM_INT);
             $consulta->execute();
             //var_dump($consulta);die();
             return $consulta;
+        } catch (PDOException $e) {
+            return false;
+        }
     }
-
 
     public function insertEstudiante($idBanner,$primerApellido,$programa,$codPrograma,$cadena,$periodo,$estado,$tipoEstudiante,$rutaAcademica,$sello,$operador,$autorizadoAsistir){
         try {

a453988b3699863ef902134a8f2e2222fb5b1080
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-24 08:14:15 -0500

    []periodos

--- app/controllers/Materiasporver.php
+++ app/controllers/Materiasporver.php
@@ -10,12 +10,7 @@ class Materiasporver extends Controller{
         $fechaActual = date('Y-m-d');
         $mesActual = date('m');
         $periodo = $this->model->getPeriodo();
-        var_dump($periodo->rowCount());die();
-        foreach ($periodo as $key => $value) {
-            $ciclo1 = explode('-', $value->fechaInicioCiclo1);
-            $ciclo2 = explode('-', $value->fechaInicioCiclo2);
-            
-        }
+        var_dump($periodo->fetch(PDO::FETCH_ASSOC));die();
         return $mesActual;
     }
 

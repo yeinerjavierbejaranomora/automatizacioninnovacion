078f2b3d4d40e12eb6ec06ae0e09c35d97fdb327
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-27 14:23:17 -0500

    [programar materias primer ciclo][get rmaterias prerequisitos]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -113,7 +113,7 @@ class Programarprimerciclo extends Controller{
                     $creditoMateria = $materia['creditos'];
                     $ciclo = $materia['ciclo'];
                     $prerequisitosConsulta = $this->model->prerequisitos($codMateria,$programa);
-                    echo $prerequisitosConsulta->fetch(PDO::FETCH_ASSOC);
+                    echo $codMateria."-". $prerequisitosConsulta->fetch(PDO::FETCH_ASSOC);
                 endforeach;
                 die();
             endforeach;

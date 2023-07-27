4008f3d2f80075ffc9668b62da6e2faa5657507e
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-27 09:03:15 -0500

    [programar materias primer ciclo]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -1,6 +1,13 @@
 <?php
 class Programarprimerciclo extends Controller{
 
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = $this->model("ProgramarPrimerCicloModel");
+    }
+    
     public function primerciclo(){
         echo "Hola ppc";
     }

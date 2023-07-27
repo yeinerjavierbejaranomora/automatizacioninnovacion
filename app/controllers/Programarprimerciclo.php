d6c9b42ac4a029cda48fdb26e939d4cc21ab35af
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-27 15:55:17 -0500

    [programar materias primer ciclo][]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -99,7 +99,7 @@ class Programarprimerciclo extends Controller{
                 $reglasNegocioConsulta = $this->model->getReglasNegocio($programa,$ruta,$tipoEstudiante,$cicloReglaNegocio);
                 $reglasNegocio = $reglasNegocioConsulta->fetchAll(PDO::FETCH_ASSOC);
                 $numeroCreditosPermitidos = $reglasNegocio['creditos'];
-                $numeroMateriasPermitidos = (int)$reglasNegocio['materiasPermitidas'];
+                $numeroMateriasPermitidos = $reglasNegocio['materiasPermitidas'];
                 var_dump($numeroMateriasPermitidos);die();
                 $orden = 1;
                 //var_dump($programa,$materiasPorVer->fetchAll());die();

a7b10521565c95d4f1ff791fbee4a9e5bc5b6399
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 11:08:30 -0500

    [programar materias primer ciclo][for]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -138,7 +138,7 @@ class Programarprimerciclo extends Controller{
                     /*$prerequisitosConsulta = $this->model->prerequisitos($codMateria,$programa);
                     $prerequisitos = $prerequisitosConsulta->fetch(PDO::FETCH_ASSOC)['prerequisito'];*/
                     //echo $codMateria."-". $prerequisitos."<br>";
-                    var_dump($prerequisitos);
+                    var_dump($prerequisitos,"<br>");
                     /*if ($prerequisitos == '' && $ciclo != 2 && $cuentaCursosCiclo1 < $numeroMateriasPermitidos) :
                         $estaPlaneacion = $this->model->estaPlaneacion($codMateria,$codBanner);
                         if ($estaPlaneacion->rowCount() == 0  && $numeroCreditos < $numeroCreditosPermitidos) :

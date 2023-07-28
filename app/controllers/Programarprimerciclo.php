2add4fe7c6c258743026f5cb8d31817358e960b7
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 14:22:11 -0500

    [programar materias primer ciclo][for]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -41,7 +41,7 @@ class Programarprimerciclo extends Controller{
         $marcaIngreso = trim($marcaIngreso, ",");
         $estudiantes = $this->model->getEstudiantesNum($offset,$marcaIngreso);
         var_dump($estudiantes->rowCount());die();
-        $limit = 50;
+        $limit = 300;
         $numEstudinates = ceil($estudiantes->rowCount()/$limit);
         for ($i=0; $i < $numEstudinates; $i++) { 
             //sleep(10);

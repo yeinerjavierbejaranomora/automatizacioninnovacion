ab30d6d5bd269db7c9dc67cec5c6ec99a9093fc8
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 09:12:02 -0500

    [programar materias primer ciclo][]

--- app/models/ProgramarPrimerCicloModel.php
+++ app/models/ProgramarPrimerCicloModel.php
@@ -120,6 +120,7 @@ class ProgramarPrimerCicloModel{
             $consultaEstaPlaneacion->bindParam(1,$codMateria,PDO::PARAM_STR);
             $consultaEstaPlaneacion->bindParam(2,$codBanner,PDO::PARAM_INT);
             $consultaEstaPlaneacion->execute();
+            var_dump($consultaEstaPlaneacion->fetch(PDO::FETCH_ASSOC));die();
             return $consultaEstaPlaneacion;
         } catch (PDOException $e) {
             return false;

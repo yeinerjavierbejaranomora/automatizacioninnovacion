60f3c2f0fc0eb2fc1ebd3a6fbbe37962a61f27cd
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 10:16:44 -0500

    [programar materias primer ciclo][]

--- app/models/ProgramarPrimerCicloModel.php
+++ app/models/ProgramarPrimerCicloModel.php
@@ -37,7 +37,7 @@ class ProgramarPrimerCicloModel{
 
     public function logAplicacion($accion,$tabla){
         try {
-            $consultaLog = $this->db->connect()->prepare("SELECT * FROM `logaplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
+            $consultaLog = $this->db->connect()->prepare("SELECT * FROM `logAplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
             $consultaLog->bindValue(1,$accion,PDO::PARAM_STR);
             $consultaLog->bindValue(2,$tabla,PDO::PARAM_STR);
             $consultaLog->execute();

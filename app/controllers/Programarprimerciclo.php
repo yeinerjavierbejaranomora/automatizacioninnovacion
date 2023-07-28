a67b5b2573499a291003914e3f2083fdc54ab806
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 09:35:44 -0500

    [programar materias primer ciclo][]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -125,7 +125,7 @@ class Programarprimerciclo extends Controller{
                             $cuentaCursosCiclo1++;
                         endif;*/
                     else:
-                        //var_dump("Cp",$numeroCreditos,$creditoMateria,$prerequisitos);       
+                        var_dump("Cp",$numeroCreditos,$creditoMateria,$prerequisitos);die();       
                         $prerequisitos = $prerequisitos;
                         $estaPlaneacion = $this->model->estaPlaneacionPrerequisitos($prerequisitos,$codBanner);
                         $estaPorVer = $this->model->estaPorVer($prerequisitos,$codBanner);

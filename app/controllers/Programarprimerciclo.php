226342818154430c61fd72227f03fe5e7c837f02
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-28 11:32:25 -0500

    [programar materias primer ciclo][for]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -67,7 +67,7 @@ class Programarprimerciclo extends Controller{
         else :
             $offset = $log->fetch(PDO::FETCH_ASSOC)['idFin'];
         endif;
-        $limit = 100;
+        $limit = 50;
         $estudiantes = $this->model->getEstudiantes($offset,$marcaIngreso,$limit);
         if($estudiantes->rowCount() > 0):
             foreach($estudiantes as $estudiante):

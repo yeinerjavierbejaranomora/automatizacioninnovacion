7c3e63feb3a4da2f8216e0476a4a9759e4baa4e0
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-01 09:26:46 -0500

    [mod estudiantes pregrado ]

--- app/controllers/Mafireplica.php
+++ app/controllers/Mafireplica.php
@@ -15,7 +15,7 @@ class Mafireplica  extends Controller{
         else:
             $offset = 0;
         endif;
-        $limit = 4000;
+        $limit = 200;
         $datosNum = $this->model->numeroDatosMafi($offset);
         $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);
         if ($datosNumFetch['totalEstudiantes'] > 0) :

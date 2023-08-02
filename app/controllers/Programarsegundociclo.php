7cfb31bf298dbeb64beae4dc92563fd60e752230
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-02 12:32:18 -0500

    [programar materias segundo ciclo][]

--- app/controllers/Programarsegundociclo.php
+++ app/controllers/Programarsegundociclo.php
@@ -22,7 +22,7 @@ class Programarsegundociclo extends Controller{
         else :
             $offset = $log->fetch(PDO::FETCH_ASSOC)['idFin'];
         endif;
-        $limit = 300;
+        $limit = 100;
         $estudiantes = $this->model->getEstudiantes($offset,$marcaIngreso,$limit);
         if($estudiantes->rowCount() > 0):
             foreach ($estudiantes as $key => $estudiante) {

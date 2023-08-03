583b53c4beb18657d2eced9790a67e17421e5965
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-03 15:59:05 -0500

    [ajustes PROGRAMAR PRIMER CICLO][prueba envio masivo][eliminar insercion a indice]

--- app/models/ProgramarPrimerCicloModel.php
+++ app/models/ProgramarPrimerCicloModel.php
@@ -8,7 +8,7 @@ class ProgramarPrimerCicloModel{
 
     public function periodos(){
         try {
-            $consultaPeriodo = $this->db->connect()->prepare("SELECT `periodoActivo` FROM `periodo` WHERE `periodoActivo` = 1");
+            $consultaPeriodo = $this->db->connect()->prepare("SELECT `periodos` FROM `periodo` WHERE `periodoActivo` = 1");
             $consultaPeriodo->execute();
             return $consultaPeriodo;
         } catch (PDOException $e) {

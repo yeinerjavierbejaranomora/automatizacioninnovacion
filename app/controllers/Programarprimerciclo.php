dc4a8679de8d9e79eb3abf0cd97d39773c51764a
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-03 15:02:50 -0500

    [ajustes PROGRAMAR PRIMER CICLO][prueba envio masivo]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -40,7 +40,7 @@ class Programarprimerciclo extends Controller{
         }
         $marcaIngreso = trim($marcaIngreso, ",");
         $estudiantes = $this->model->getEstudiantesNum($offset,$marcaIngreso);
-        $limit = 50;
+        $limit = 1000;
         $numEstudinates = ceil($estudiantes->rowCount()/$limit);
         $programacion = [];
         for ($i=0; $i < $numEstudinates; $i++) { 

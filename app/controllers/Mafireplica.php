44d510c512f3bdd6550c652b07cfdf59f7ee6a22
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-04 09:13:16 -0500

    [modificacion limite datos mafi replica]

--- app/controllers/Mafireplica.php
+++ app/controllers/Mafireplica.php
@@ -16,7 +16,7 @@ class Mafireplica  extends Controller{
             $offset = 0;
         endif;
         // var_dump($offset);die();
-        $limit = 5000;
+        $limit = 6000;
         $datosNum = $this->model->numeroDatosMafi($offset);
         // var_dump($datosNum->fetchAll());die();
         $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);

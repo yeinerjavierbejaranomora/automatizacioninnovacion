59a7434de2b2a399d1fa688226c8399f58c76707
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-09 14:13:29 -0500

    [materias por ver estudiantes antiguos][historial moodle]

--- app/controllers/Materiasporver.php
+++ app/controllers/Materiasporver.php
@@ -146,7 +146,7 @@ class Materiasporver extends Controller{
                 $mallaCurricular = $this->model->baseAcademica($codBanner,$programa,$periodo);
                 $historial = $this->model->historial($codBanner);
                 $historialMoodle = $this->model->historialMoodle($codBanner);
-                var_dump($historialMoodle);die();
+                var_dump(count($historialMoodle));die();
                 $diff = array_udiff($mallaCurricular, $historial, function($a, $b) {
                     return $a['codMateria'] <=> $b['codMateria'];
                 });

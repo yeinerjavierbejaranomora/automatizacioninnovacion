e4f26d6268fd62a505b7df0c3f10a5e9c9217cdc
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-09 10:26:49 -0500

    [materias por ver estudiantes transferentes][hstorial moodle]

--- app/controllers/Materiasporver.php
+++ app/controllers/Materiasporver.php
@@ -76,7 +76,7 @@ class Materiasporver extends Controller{
                 $diffMoodle = array_udiff($diff, $historialMoodle, function($a, $b) {
                     return $a['codMateria'] <=> $b['codMateria'];
                 });
-                var_dump(count($diffMoodle));die();
+                var_dump(count($historialMoodle));die();
                 $insertMateriaPorVer = $this->model->insertMateriaPorVer($diff);
                 $registroMPV = $registroMPV + $insertMateriaPorVer;
                 if(count($diff) == $insertMateriaPorVer):

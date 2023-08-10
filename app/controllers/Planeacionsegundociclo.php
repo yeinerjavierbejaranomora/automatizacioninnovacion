7fca644f7ba1a3619b46e79f139faa931af2cd3e
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 16:11:03 -0500

    [planeacion segundo ciclo]

--- app/controllers/Planeacionsegundociclo.php
+++ app/controllers/Planeacionsegundociclo.php
@@ -193,4 +193,133 @@ class Planeacionsegundociclo extends Controller{
             echo "No hay estudiantes de segundo ciclo para programar <br>";
         endif;
     }
+
+    public function programarOrden($estudiante){
+        $fechaInicio = date('Y-m-d H:i:s');
+        $primerId = $estudiante['id'];
+        $ultimoRegistroId = 0;
+        $idHomologante = $estudiante['id'];
+        $codHomologante = $estudiante['homologante'];
+        $programaHomologante = $estudiante['programa'];
+        $tipoEstudiante = $estudiante['tipo_estudiante'];
+        $materiasPlaneadas = $this->model->materiasPlaneadas($codHomologante, $programaHomologante);
+        $materias_planeadas = '';
+        foreach ($materiasPlaneadas as $materia) :
+            $codmateria = $materia['codMateria'];
+            $materias_planeadas = $materias_planeadas . "'" . $codmateria . "',";
+        endforeach;
+        $materias_planeadas = substr($materias_planeadas, 0, -1);
+        $materias_planeadas = ($materias_planeadas == '') ? "'n-a'" : $materias_planeadas;
+        $consultaMateriasPorVer = $this->model->materiasPorVer($codHomologante,$programaHomologante,$materias_planeadas);
+        $numeroCreditos = $this->model->getCreditosplaneados($codHomologante);
+        $numeroCreditos = $numeroCreditos->rowCount() == 0 ? 0 : $numeroCreditos->fetch(PDO::FETCH_ASSOC)['CreditosPlaneados'];
+        $numeroMateriasPorVer = $consultaMateriasPorVer->rowCount();
+        $ruta = $estudiante['bolsa'];
+        if ($ruta != '') :
+            $ruta = 1;
+        else :
+            $ruta = 0;
+        endif;
+        $tipoEstudiante = $estudiante['tipo_estudiante'];
+
+        switch ($tipoEstudiante) {
+            case str_contains($tipoEstudiante, 'TRANSFERENTE'):
+                $tipoEstudiante = 'TRANSFERENTE';
+                break;
+            case str_contains($tipoEstudiante, 'ESTUDIANTE ANTIGUO'):
+                $tipoEstudiante = 'ESTUDIANTE ANTIGUO';
+                break;
+            case str_contains($tipoEstudiante, 'PRIMER INGRESO'):
+                $tipoEstudiante = 'PRIMER INGRESO';
+                break;
+            case str_contains($tipoEstudiante, 'PSEUDO ACTIVOS'):
+                $tipoEstudiante = 'ESTUDIANTE ANTIGUO';
+                break;
+            case str_contains($tipoEstudiante, 'REINGRESO'):
+                $tipoEstudiante = 'ESTUDIANTE ANTIGUO';
+                break;
+            case str_contains($tipoEstudiante, 'INGRESO SINGULAR'):
+                $tipoEstudiante = 'PRIMER INGRESO';
+                break;
+
+            default:
+                # code...
+                break;
+        }
+        $cicloReglaNegocio = 2;
+        $reglasNegocioConsulta = $this->model->getReglasNegocio($programaHomologante, $ruta, $tipoEstudiante, $cicloReglaNegocio);
+        $reglasNegocio = $reglasNegocioConsulta->fetch(PDO::FETCH_ASSOC);
+        $numeroCreditosPermitidos = $reglasNegocio['creditos'];
+        $numeroMateriasPermitidos = (int)$reglasNegocio['materiasPermitidas'];
+        $orden2 = 1;
+        if ($numeroMateriasPorVer == 0) :
+            $mensajeAlerta = 'El estudiante con idBanner' . $codHomologante . ', perteneciente al programa '.$programaHomologante.', no tiene materias por ver, segundo ciclo.';
+            $insertarAlertaTemprana = $this->model->insertarAlerta($codHomologante, $tipoEstudiante, $mensajeAlerta);
+            $updateEstudinate = $this->model->updateEstudinate($idHomologante, $codHomologante);
+            $ultimoRegistroId = $estudiante['id'];
+            $idBannerUltimoRegistro = $estudiante['homologante'];
+            $fechaFin = date('Y-m-d H:i:s');
+            $acccion = 'Insert-PlaneacionSegundoCiclo';
+            $tablaAfectada = 'planeacion';
+            $descripcion = 'Se realizo la insercion en la tabla planeacion insertando las materias del segundo ciclo del estudiante ' . $codHomologante . ', iniciando en el id ' . $primerId . ' y terminando en el id ' . $ultimoRegistroId . '.';
+            $fecha = date('Y-m-d H:i:s');
+            $insertarLogAplicacion = $this->model->insertarLogAplicacion($primerId, $ultimoRegistroId, $fechaInicio, $fechaFin, $acccion, $tablaAfectada, $descripcion);
+            //$insertIndiceCambio = $this->model->insertIndiceCambio($idBannerUltimoRegistro, $acccion, $descripcion, $fecha);
+            echo "Sin  Materias : " . $codHomologante . "<br />";
+        else :
+            foreach ($consultaMateriasPorVer as $materia) :
+                $codBanner = $materia['codBanner'];
+                $codMateria = $materia['codMateria'];
+                $creditoMateria = $materia['creditos'];
+                $ciclo = $materia['ciclo'];
+                $prerequisitos = $materia['prerequisito'];
+                $numeroCreditosTemp = $numeroCreditos + $creditoMateria;
+                if ($numeroCreditosTemp>=$numeroCreditosPermitidos) :
+                    break;
+                endif;
+                if($prerequisitos == '' && $numeroCreditosTemp<=$numeroCreditosPermitidos):
+                    $consultaEstaPlaneacion = $this->model->estaPlaneacion($codMateria,$codBanner);
+                    $fetchEstaPlaneacion = $consultaEstaPlaneacion->fetchAll();
+                    $codBanner = $codBanner;
+                    $planeada = $fetchEstaPlaneacion['codMateria'];
+                    if($planeada == '' && $numeroCreditos < $numeroCreditosPermitidos && $numeroCreditosTemp<=$numeroCreditosPermitidos):
+                        $numeroCreditos = $numeroCreditos + $creditoMateria;
+                        $semestre = 1;
+                        $programada = '';
+                        $insertPlaneada = $this->model->insertarPlaneacion($codBanner,$codMateria,$orden2,$semestre,$programada,$programaHomologante);
+                        //echo $insertPlaneada . "<br />";
+                    endif;
+                else:
+                    $prerequisitos = trim($prerequisitos,'"');
+                    $prerequisitos = '"' . $prerequisitos . '"';
+                    $consultaEstaPlaneacion = $this->model->estaPlaneacionPrerequisitos($prerequisitos, $codBanner);
+                    $fetchEstaPlaneacion = $consultaEstaPlaneacion->fetch(PDO::FETCH_ASSOC);
+                    $preprogramado = $fetchEstaPlaneacion['codMateria'];
+                    $consultaEstaPorVer = $this->model->estaPorVer($prerequisitos, $codBanner);
+                    $fetchEstaPorVer = $consultaEstaPorVer->fetch(PDO::FETCH_ASSOC);
+                    $estaPorVer = $fetchEstaPorVer['codMateria'];
+                    if($preprogramado == '' && $estaPorVer == '' && $numeroCreditosTemp<=$numeroCreditosPermitidos &&  $numeroCreditos < $numeroCreditosPermitidos):
+                        $numeroCreditos = $numeroCreditos + $creditoMateria;
+                        $semestre = 1;
+                        $programada = '';
+                        $insertPlaneada = $this->model->insertarPlaneacion($codBanner,$codMateria,$orden2,$semestre,$programada,$programaHomologante);
+                        //echo $insertPlaneada . "<br />";
+                    endif;
+                endif;
+            endforeach;
+            $orden2++;
+            $updateEstudinate = $this->model->updateEstudinate($idHomologante, $codHomologante);
+            $ultimoRegistroId = $estudiante['id'];
+            $idBannerUltimoRegistro = $estudiante['homologante'];
+            $fechaFin = date('Y-m-d H:i:s');
+            $acccion = 'Insert-PlaneacionSegundoCiclo';
+            $tablaAfectada = 'planeacion';
+            $descripcion = 'Se realizo la insercion en la tabla planeacion insertando las materias del segundo ciclo del estudiante ' . $codBanner . ', perteneciente al programa '.$programaHomologante.', iniciando en el id ' . $primerId . ' y terminando en el id ' . $ultimoRegistroId . '.';
+            $fecha = date('Y-m-d H:i:s');
+            $insertarLogAplicacion = $this->model->insertarLogAplicacion($primerId, $ultimoRegistroId, $fechaInicio, $fechaFin, $acccion, $tablaAfectada, $descripcion);
+            //$insertIndiceCambio = $this->model->insertIndiceCambio($idBannerUltimoRegistro, $acccion, $descripcion, $fecha);
+            echo $ultimoRegistroId . "-Fecha Inicio: " . $fechaInicio . "Fecha Fin: " . $fechaFin . "<br>";
+                    // echo "Planeación realizada para : " . $codBanner . " y " . $codMateria . "-".$fechaInicio."-".$fechaFin. "<br />";
+        endif;
+    }
 }
--- app/models/PlaneacionSegundoCicloModel.php
+++ app/models/PlaneacionSegundoCicloModel.php
@@ -63,4 +63,147 @@ class PlaneacionSegundoCicloModel{
             return false;
         }
     }
+
+    public function materiasPlaneadas($codHomologante,$programaHomologante){
+        try {
+            //$consultaMateriasplaneadas = $this->db->connect()->prepare("SELECT p.codBanner, p.codMateria FROM planeacion p INNER JOIN mallaCurricular mc ON p.codMateria=mc.codigoCurso WHERE codBanner=? AND p.codprograma = ? AND mc.codprograma = ?");
+            $consultaMateriasplaneadas = $this->db->connect()->prepare("SELECT p.codMateria FROM planeacion p INNER JOIN mallaCurricular mc ON p.codMateria=mc.codigoCurso WHERE codBanner=? AND p.codprograma = ? AND mc.codprograma = ?");
+            $consultaMateriasplaneadas->bindValue(1,$codHomologante,PDO::PARAM_INT);
+            $consultaMateriasplaneadas->bindValue(2,$programaHomologante,PDO::PARAM_STR);
+            $consultaMateriasplaneadas->bindValue(3,$programaHomologante,PDO::PARAM_STR);
+            $consultaMateriasplaneadas->execute();
+            return $consultaMateriasplaneadas;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function materiasPorVer($codHomologante,$programaHomologante,$materias_planeadas){
+        try {
+            $consultaMateriasPorVer = $this->db->connect()->prepare("SELECT mv.codBanner, mv.codMateria, mv.orden, mc.creditos, mc.ciclo,mc.prerequisito  FROM materiasPorVer mv INNER JOIN mallaCurricular mc ON mv.codMateria=mc.codigoCurso WHERE codBanner=? AND mv.codprograma = ? AND mc.codprograma = ? AND mv.codMateria NOT IN ($materias_planeadas) ORDER BY mv.orden ASC");
+            $consultaMateriasPorVer->bindParam(1,$codHomologante,PDO::PARAM_INT);
+            $consultaMateriasPorVer->bindParam(2,$programaHomologante,PDO::PARAM_STR);
+            $consultaMateriasPorVer->bindParam(3,$programaHomologante,PDO::PARAM_STR);
+            $consultaMateriasPorVer->execute();
+            return $consultaMateriasPorVer;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getCreditosplaneados($codHomologante){
+        try {
+            $consultaCreditosplaneados = $this->db->connect()->prepare("SELECT p.codBanner, SUM(mc.creditos) AS CreditosPlaneados FROM mallaCurricular mc INNER JOIN planeacion p ON mc.codigoCurso=p.codMateria WHERE p.codBanner=? AND mc.codprograma=p.codprograma group by p.codbanner");
+            $consultaCreditosplaneados->bindValue(1,$codHomologante,PDO::PARAM_INT);
+            $consultaCreditosplaneados->execute();
+            return $consultaCreditosplaneados;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getReglasNegocio($programa,$ruta,$tipoEstudiante,$cicloReglaNegocio){
+        try {
+            $consultaReglasNegocio = $this->db->connect()->prepare("SELECT `creditos`,`materiasPermitidas` FROM `reglasNegocio` WHERE `programa` = ? AND `ruta` = ? AND `tipoEstudiante` = ? AND `ciclo` =? AND `activo` = 1");
+            $consultaReglasNegocio->bindParam(1,$programa,PDO::PARAM_INT);
+            $consultaReglasNegocio->bindParam(2,$ruta,PDO::PARAM_INT);
+            $consultaReglasNegocio->bindParam(3,$tipoEstudiante,PDO::PARAM_STR);
+            $consultaReglasNegocio->bindParam(4,$cicloReglaNegocio,PDO::PARAM_INT);
+            $consultaReglasNegocio->execute();
+            return $consultaReglasNegocio;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function insertarAlerta($codigoBanner,$tipoEstudiante,$mensajeAlerta){
+        try {
+            $insertAlerta = $this->db->connect()->prepare("INSERT INTO `alertas_tempranas` SET `idbanner` = ?, `tipo_estudiante` = ?, `desccripcion` = ?, `created_at` = NOW(), `updated_at` = NOW()");
+            $insertAlerta->bindValue(1,$codigoBanner,PDO::PARAM_INT);
+            $insertAlerta->bindValue(2,$tipoEstudiante,PDO::PARAM_STR);
+            $insertAlerta->bindValue(3,$mensajeAlerta,PDO::PARAM_STR);
+            $insertAlerta->execute();
+            return $insertAlerta;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function updateEstudinate($idHomologante,$codHomologante){
+        try {
+            $updateEstudiante = $this->db->connect()->prepare("UPDATE `estudiantes` SET `planeado_ciclo2`='OK' WHERE `id` = ? AND `homologante` = ?");
+            $updateEstudiante->bindParam(1,$idHomologante,PDO::PARAM_INT);
+            $updateEstudiante->bindParam(2,$codHomologante,PDO::PARAM_INT);
+            $updateEstudiante->execute();
+            if ($updateEstudiante == true):
+                return true;
+            endif;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function insertarLogAplicacion($primerID,$ultimoRegistroId,$fechaInicio,$fechaFin,$acccion,$tablaAfectada,$descripcion){
+        try {
+            $fecha = date('Y-m-d H:i:s');
+            $insertarLog = $this->db->connect()->prepare("INSERT INTO `logAplicacion` SET
+                                                                        `idInicio` = ?, 
+                                                                        `idFin` = ?, 
+                                                                        `fechaInicio` = ?, 
+                                                                        `fechaFin` = ?, 
+                                                                        `accion` = ?, 
+                                                                        `tabla_afectada` = ?, 
+                                                                        `descripcion` = ?, 
+                                                                        `created_at` = ?, 
+                                                                        `updated_at` = ?");
+            $insertarLog->bindValue(1,$primerID,PDO::PARAM_INT);
+            $insertarLog->bindValue(2,$ultimoRegistroId,PDO::PARAM_STR);
+            $insertarLog->bindValue(3,$fechaInicio,PDO::PARAM_STR);
+            $insertarLog->bindValue(4,$fechaFin,PDO::PARAM_STR);
+            $insertarLog->bindValue(5,$acccion,PDO::PARAM_STR);
+            $insertarLog->bindValue(6,$tablaAfectada,PDO::PARAM_STR);
+            $insertarLog->bindValue(7,$descripcion,PDO::PARAM_STR);
+            $insertarLog->bindValue(8,$fecha,PDO::PARAM_STR);
+            $insertarLog->bindValue(9,$fecha,PDO::PARAM_STR);
+            $insertarLog->execute();
+            return $insertarLog;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function estaPlaneacion($codMateria,$codBanner){
+        //var_dump($codMateria,$codBanner);die();
+        try {
+            $consultaEstaPlaneacion= $this->db->connect()->prepare("SELECT codMateria FROM planeacion WHERE codMateria=? AND codBanner=?");
+            $consultaEstaPlaneacion->bindValue(1,$codMateria,PDO::PARAM_STR);
+            $consultaEstaPlaneacion->bindValue(2,$codBanner,PDO::PARAM_INT);
+            $consultaEstaPlaneacion->execute();
+            return $consultaEstaPlaneacion;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function estaPlaneacionPrerequisitos($prerequisitos,$codBanner){
+        try {
+            $consultaEstaPlaneacionPrerequisitos = $this->db->connect()->prepare("SELECT `codMateria` FROM `planeacion` WHERE `codMateria` IN ($prerequisitos)  AND `codBanner` = ?");
+            $consultaEstaPlaneacionPrerequisitos->bindParam(1,$codBanner,PDO::PARAM_STR);
+            $consultaEstaPlaneacionPrerequisitos->execute();
+            return $consultaEstaPlaneacionPrerequisitos;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function estaPorVer($prerequisitos,$codBanner){
+        try {
+            $consultaEstaPorVer = $this->db->connect()->prepare("SELECT `codMateria` FROM `materiasPorVer` WHERE `codMateria` IN ($prerequisitos)  AND `codBanner` = ? ORDER BY `id` ASC LIMIT 1");
+            $consultaEstaPorVer->bindParam(1,$codBanner,PDO::PARAM_STR);
+            $consultaEstaPorVer->execute();
+            return $consultaEstaPorVer;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }

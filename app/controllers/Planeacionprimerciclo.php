5b9664e75311351b1fc5d97635105e39df2c852d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 15:13:14 -0500

    [planeacion primer ciclo]

--- app/controllers/Planeacionprimerciclo.php
+++ app/controllers/Planeacionprimerciclo.php
@@ -7,6 +7,27 @@ class Planeacionprimerciclo extends Controller{
     }
 
     public function inicio(){
-        echo "Planeacion Primer Ciclo";
+        $log = $this->model->logAplicacion('Insert-PlaneacionPrimerCiclo', 'planeacion');
+        if ($log->rowCount() == 0) :
+            $offset = 0;
+        else :
+            $offset = $log->fetch(PDO::FETCH_ASSOC)['idFin'];
+        endif;
+        // $offset = 6013;
+        $periodos = $this->model->periodos();
+        $marcaIngreso = "";
+        foreach ($periodos as $periodo) {
+            $marcaIngreso .= (int)$periodo['periodos'] . ",";
+        }
+        $marcaIngreso = trim($marcaIngreso, ",");
+        // var_dump($marcaIngreso);die();
+        $estudiantes = $this->model->getEstudiantesNum($offset,$marcaIngreso);
+        var_dump($estudiantes->rowCount());die();
+        /*$limit = 500;
+        $numEstudinates = ceil($estudiantes->rowCount()/$limit);
+        for ($i=0; $i < $numEstudinates; $i++) { 
+            //sleep(10);
+            $this->primerciclo($limit,$marcaIngreso);
+        }*/
     }
 }
--- app/models/PlaneacionPrimerCicloModel.php
+++ app/models/PlaneacionPrimerCicloModel.php
@@ -5,4 +5,33 @@ class PlaneacionPrimerCicloModel{
     {
         $this->db = new Database();
     }
+
+    public function periodos(){
+        try {
+            $consultaPeriodo = $this->db->connect()->prepare("SELECT `periodos` FROM `periodo` WHERE `periodoActivo` = 1");
+            $consultaPeriodo->execute();
+            return $consultaPeriodo;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getEstudiantesNum($offset,$marcaIngreso){
+        //var_dump($marcaIngreso);die();
+        try {
+            $consultaEstudiantes = $this->db->connect()->prepare("SELECT `id`,`homologante` FROM `estudiantes` 
+            WHERE `id` > ?
+            AND `materias_faltantes` = 'OK' 
+            AND `planeado_ciclo1` IS NULL 
+            AND `planeado_ciclo2` IS NULL 
+            AND `marca_ingreso` IN ($marcaIngreso) 
+            /*AND `programa` != 'PPSV'*/ 
+            ORDER BY `id` ASC");
+            $consultaEstudiantes->bindParam(1,$offset,PDO::PARAM_INT);
+            $consultaEstudiantes->execute();
+            return $consultaEstudiantes;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }

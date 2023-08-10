ede4e150a5d85c9adacd403676829eaa95392758
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 11:32:37 -0500

    [metodo para programar segundo ciclo estudinates de PPSV][llamar funcion programarOrden]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -137,7 +137,7 @@ class Programarprimerciclo extends Controller{
                         if ($cuentaCursosCiclo1 >= $numeroMateriasPermitidos) :
                             break;
                         endif;
-                        var_dump($materia,$materia['prerequisito']);
+                        var_dump($materia['prerequisito']);
                         /*$codBanner = $materia['codBanner'];
                         $codMateria = $materia['codMateria'];
                         $creditoMateria = $materia['creditos'];

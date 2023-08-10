09ead61f441ca2f5cfd9f18cc68101d9fb183036
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 13:46:30 -0500

    [metodo para programar segundo ciclo estudinates de PPSV][llamar funcion programarOrden]

--- app/controllers/Programarsegundociclo.php
+++ app/controllers/Programarsegundociclo.php
@@ -135,7 +135,7 @@ class Programarsegundociclo extends Controller{
                 else:
                     //var_dump($consultaMateriasPorVer->fetchAll());die();
                     foreach($consultaMateriasPorVer as $materia):
-                        var_dump($materia);die();
+                        var_dump($materia);
                         $codBanner = $materia['codBanner'];
                         $codMateria = $materia['codMateria'];
                         $creditoMateria = $materia['creditos'];

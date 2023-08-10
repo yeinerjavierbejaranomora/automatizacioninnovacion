a4875a697bec40c4518fb3657d634249db26520b
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 15:57:53 -0500

    [planeacion primer ciclo]

--- app/controllers/Planeacionsegundociclo.php
+++ app/controllers/Planeacionsegundociclo.php
@@ -5,7 +5,28 @@ class Planeacionsegundociclo extends Controller{
         $this->model= $this->model('PlaneacionSegundoCicloModel');
     }
 
+
     public function inicio(){
-        echo "Planeacion Segundo Ciclo ";
+        $periodos = $this->model->periodos();
+        $marcaIngreso = "";
+        foreach ($periodos as $periodo) {
+            $marcaIngreso .= (int)$periodo['periodos'] . ",";
+        }
+        $marcaIngreso = trim($marcaIngreso, ",");
+        // var_dump($marcaIngreso);die();
+        $log = $this->model->logAplicacion('Insert-PlaneacionSegundoCiclo', 'planeacion');
+        if ($log->rowCount() == 0) :
+            $offset = 0;
+        else :
+            $offset = $log->fetch(PDO::FETCH_ASSOC)['idFin'];
+        endif;
+        $limit = 500;
+        $estudiantes = $this->model->getEstudiantesNum($offset,$marcaIngreso);
+        $numEstudiantes = $estudiantes->rowCount();
+        $divEstudiantes = ceil($numEstudiantes/$limit);
+        //var_dump($numEstudiantes);die();
+        for ($i=0; $i < $divEstudiantes; $i++) {
+            $this->segundociclo($marcaIngreso,$limit);
+        }
     }
 }
--- app/models/PlaneacionSegundoCicloModel.php
+++ app/models/PlaneacionSegundoCicloModel.php
@@ -5,4 +5,43 @@ class PlaneacionSegundoCicloModel{
     {
         $this->db = new Database();
     }
+
+    public function periodos(){
+        try {
+            $consultaPeriodo = $this->db->connect()->prepare("SELECT `periodos` FROM `periodo` WHERE `periodoActivo` = 1");
+            $consultaPeriodo->execute();
+            return $consultaPeriodo;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function logAplicacion($accion,$tabla){
+        try {
+            $consultaLog = $this->db->connect()->prepare("SELECT `idFin` FROM `logAplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
+            $consultaLog->bindValue(1,$accion,PDO::PARAM_STR);
+            $consultaLog->bindValue(2,$tabla,PDO::PARAM_STR);
+            $consultaLog->execute();
+            return $consultaLog;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getEstudiantesNum($offset,$marcaIngreso){
+        try {
+            $consultaEstudiantes = $this->db->connect()->prepare("SELECT id, homologante, programa,tipo_estudiante FROM estudiantes 
+            WHERE `id` > ?
+            AND materias_faltantes='OK' 
+            AND planeado_ciclo1='OK' 
+            AND planeado_ciclo2 IS NULL 
+            AND marca_ingreso IN ($marcaIngreso) 
+            ORDER BY id ASC");
+            $consultaEstudiantes->bindParam(1,$offset,PDO::PARAM_INT);
+            $consultaEstudiantes->execute();
+            return $consultaEstudiantes;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }

5ad9913ec5b544ccb80a42583c1543bdabd91186
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 11:49:13 -0500

    [metodo para programar segundo ciclo estudinates de PPSV][llamar funcion programarOrden]

--- app/controllers/Programarsegundociclo.php
+++ app/controllers/Programarsegundociclo.php
@@ -53,11 +53,8 @@ class Programarsegundociclo extends Controller{
         if($estudiantes->rowCount() > 0):
             foreach ($estudiantes as $key => $estudiante) {
                 $programaHomologante = $estudiante['programa'];
-                if($programaHomologante != 'PPSV'):
-                    echo "No es PPSV <br>";
-                else:
-                    echo "1Es PPSV <br>";
-                endif;
+                if ($programaHomologante != 'PPSV') :
+
                 //var_dump($estudiante);die();
                 /*$fechaInicio = date('Y-m-d H:i:s');
                 $primerId = $estudiante['id'];
@@ -145,7 +142,7 @@ class Programarsegundociclo extends Controller{
                         /*$consultaPrerequisitos = $this->model->consultaPrerequisitos($codMateria,$programaHomologante);
                         $fetchPrerequisistos = $consultaPrerequisitos->fetch(PDO::FETCH_ASSOC);
                         $prerequisitos =  $fetchPrerequisistos['prerequisito'];*/
-                        /*$prerequisitos = $materia['prerequisito'];
+                /*$prerequisitos = $materia['prerequisito'];
 
                         $numeroCreditosTemp = $numeroCreditos + $creditoMateria;
                         if ($numeroCreditosTemp>=$numeroCreditosPermitidos) :
@@ -196,11 +193,17 @@ class Programarsegundociclo extends Controller{
                     echo $ultimoRegistroId . "-Fecha Inicio: " . $fechaInicio . "Fecha Fin: " . $fechaFin . "<br>";
                     // echo "Planeación realizada para : " . $codBanner . " y " . $codMateria . "-".$fechaInicio."-".$fechaFin. "<br />";
                 endif;*/
-
+                else :
+                    $this->programarOrden($estudiante);
+                endif;
             }
             die();
         else:
             echo "No hay estudiantes de segundo ciclo para programar <br>";
         endif;
+    }
+
+    public function programarOrden($estudiante){
+        var_dump($estudiante);die();
     }
 }

b155eae1a50286af475a943390907be6daa4e0c2
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-10 15:14:21 -0500

    [planeacion primer ciclo]

--- app/models/PlaneacionPrimerCicloModel.php
+++ app/models/PlaneacionPrimerCicloModel.php
@@ -6,6 +6,18 @@ class PlaneacionPrimerCicloModel{
         $this->db = new Database();
     }
 
+    public function logAplicacion($accion,$tabla){
+        try {
+            $consultaLog = $this->db->connect()->prepare("SELECT `idFin` FROM `logAplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
+            $consultaLog->bindValue(1,$accion,PDO::PARAM_STR);
+            $consultaLog->bindValue(2,$tabla,PDO::PARAM_STR);
+            $consultaLog->execute();
+            return $consultaLog;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
     public function periodos(){
         try {
             $consultaPeriodo = $this->db->connect()->prepare("SELECT `periodos` FROM `periodo` WHERE `periodoActivo` = 1");

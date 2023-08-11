f7c71c58a3e307d6215cdbb18a8078a9bda20e56
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 11:58:20 -0500

    [programar primer ciclo ][debug]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -104,7 +104,7 @@ class Programarprimerciclo extends Controller{
                     $materias_moodle = trim($materias_moodle, ",");
                     //var_dump($materias_moodle);die();
                     $materiasPorVer = $this->model->materiasPorVer($codigoBanner, $ciclo, $programa,$materias_moodle);
-                    var_dump($materiasPorVer->fetchAll());die();
+                    var_dump($materiasPorVer);die();
                     $numeroCreditos = $this->model->getCreditosPlaneados($codigoBanner);
                     $numeroCreditos = $numeroCreditos->rowCount() == 0 ? 0 : $numeroCreditos->fetch(PDO::FETCH_ASSOC)['CreditosPlaneados'];
                     $numeroCreditosC1 = $this->model->getCreditosCicloUno($codigoBanner);

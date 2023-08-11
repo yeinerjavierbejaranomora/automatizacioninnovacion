e70ede9e3259cf6950ba338c804c2656c32e8ea0
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 13:34:12 -0500

    [programar primer ciclo ][debug]

--- app/models/ProgramarPrimerCicloModel.php
+++ app/models/ProgramarPrimerCicloModel.php
@@ -87,10 +87,10 @@ class ProgramarPrimerCicloModel{
         try {
             $consultaMateriasPorVer = $this->db->connect()->prepare("SELECT mpv.codBanner,mpv.codMateria,mpv.orden,m.creditos,m.ciclo,m.prerequisito FROM `materiasPorVer` mpv 
             INNER JOIN mallaCurricular m ON m.codigoCurso=mpv.codMateria
-            WHERE mpv.codBanner = 100074421  
+            WHERE mpv.codBanner = ?  
             AND m.ciclo IN (1,12)
-            AND mpv.codprograma = 'PPSV'
-            AND m.codprograma = 'PPSV'
+            AND mpv.codprograma = ?
+            AND m.codprograma = ?
             AND mpv.codMateria NOT IN ('PSV22135','PSV22185')
             ORDER BY mpv.orden ASC");
             $consultaMateriasPorVer->bindValue(1,$codigoBanner,PDO::PARAM_INT);

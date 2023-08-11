7ad0dcb91eb334d99d83c129dbfa0b853de5fc98
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 09:12:56 -0500

    [programar primer ciclo ][debug]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -157,8 +157,8 @@ class Programarprimerciclo extends Controller{
                 else :
                     //$this->programarOrden($estudiante);
                 endif;
+                die();
             endforeach;
-            die();
         else:
             echo "No hay estudiantes de primer ciclo para programar <br>";
         endif;

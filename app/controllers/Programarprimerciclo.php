734a9722b5a661890dd8c101eb38dc709a1f163c
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 11:26:01 -0500

    [programar primer ciclo ][debug]

--- app/controllers/Programarprimerciclo.php
+++ app/controllers/Programarprimerciclo.php
@@ -93,12 +93,12 @@ class Programarprimerciclo extends Controller{
                     }
                     $ciclo = [1, 12];
                     $materiasMoodleConsulta = $this->model->materiasMoodle($codigoBanner);
+                    var_dump($materiasMoodleConsulta->rowCount());die();
                     $materias_moodle = "";
                     foreach ($materiasMoodleConsulta as $materia) {
                         $materias_moodle .= '"'.$materia['codigomateria'] . '",';
                     }
                     $materias_moodle = trim($materias_moodle, ",");
-                    var_dump($materias_moodle);die();
                     $materiasPorVer = $this->model->materiasPorVer($codigoBanner, $ciclo, $programa,$materias_moodle);
                     var_dump($materiasPorVer);die();
                     $numeroCreditos = $this->model->getCreditosPlaneados($codigoBanner);

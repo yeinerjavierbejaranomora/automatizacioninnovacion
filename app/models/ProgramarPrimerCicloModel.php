b5043e65cee19609c3ef1e869d20c4bae57d76ab
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 12:34:50 -0500

    [programar primer ciclo ][debug]

--- app/models/ProgramarPrimerCicloModel.php
+++ app/models/ProgramarPrimerCicloModel.php
@@ -83,7 +83,7 @@ class ProgramarPrimerCicloModel{
     }
 
     public function materiasPorVer($codigoBanner,$programa){
-        var_dump($codigoBanner,$programa);die();
+        //var_dump($codigoBanner,$programa);die();
         //try {
             $consultaMateriasPorVer = $this->db->connect()->prepare("SELECT mpv.codBanner,mpv.codMateria,mpv.orden,m.creditos,m.ciclo,m.prerequisito FROM `materiasPorVer` mpv 
             INNER JOIN mallaCurricular m ON m.codigoCurso=mpv.codMateria

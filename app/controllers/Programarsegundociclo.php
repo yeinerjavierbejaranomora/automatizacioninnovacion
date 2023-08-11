e01286f2c07220a3037f0e4c2f58e10644e5af30
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-11 15:55:15 -0500

    [programar segundo ciclo ]

--- app/controllers/Programarsegundociclo.php
+++ app/controllers/Programarsegundociclo.php
@@ -75,8 +75,9 @@ class Programarsegundociclo extends Controller{
                     }
                 endif;
                 $materias_moodle = trim($materias_moodle, ",");
-                var_dump($materias_programadas,"--",$materias_moodle);die();
-                $consultaMateriasPorVer = $this->model->materiasPorVer($codHomologante,$programaHomologante,$materias_programadas);
+                //var_dump($materias_programadas,"--",$materias_moodle);die();
+                $consultaMateriasPorVer = $this->model->materiasPorVer($codHomologante,$programaHomologante,$materias_programadas,$materias_moodle);
+                var_dump($consultaMateriasPorVer);die();
                 /*$numeroCreditos = $this->model->getCreditosplaneados($codHomologante);
                 $numeroCreditos = $numeroCreditos->rowCount() == 0 ? 0 : $numeroCreditos->fetch(PDO::FETCH_ASSOC)['CreditosPlaneados'];
                 //var_dump($numeroCreditos);die();

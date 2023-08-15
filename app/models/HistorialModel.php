553df18f8644bb68ec77e62016f566afc1a1ab61
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-15 09:10:02 -0500

    [cargar historial]

--- app/models/HistorialModel.php
+++ app/models/HistorialModel.php
@@ -12,7 +12,7 @@ class HistorialModel{
         //var_dump($fila);die();
         try {
             foreach($fila as $row):
-                var_dump($row);die();
+                var_dump($row[2]);die();
             endforeach;
         } catch (PDOException $e) {
             return false;

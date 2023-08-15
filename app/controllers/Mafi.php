90cf537cbb7e90bac957c5a2b25a0982dc85d3a9
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-15 14:38:56 -0500

    [cargar historial]

--- app/controllers/Mafi.php
+++ app/controllers/Mafi.php
@@ -21,6 +21,7 @@ class Mafi extends Controller{
         $datosNumFetch = $datosNum->fetch(PDO::FETCH_ASSOC);
         if($datosNumFetch['total'] > 0):
             $datosMafi = $this->model->dataMafi($offset);
+            var_dump($datosMafi->fetchAll());die();
             $numeroRegistros = 0;
             $primerId = $this->model->dataMafi($offset)->fetch(PDO::FETCH_ASSOC)['id'];
             $ultimoRegistroId = 0;

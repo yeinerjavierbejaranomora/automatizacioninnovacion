a2e5107d57126feaa2ded896dc8252b3fde05991
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-15 09:37:46 -0500

    [cargar historial]

--- app/models/HistorialModel.php
+++ app/models/HistorialModel.php
@@ -13,22 +13,22 @@ class HistorialModel{
         //try {
             $save = $this->db->connect()->prepare("INSERT INTO `historialAcademico` SET 
             `id` = NULL,
-            `codBanner` = ?, 
-            `nombreEst` = ?, 
-            `institucionOrigen` = ?, 
-            `codprograma` = ?, 
-            `programa` = ?, 
-            `codMateria` = ?, 
-            `nombreMat` = ?, 
-            `nota` = ?");
-            $save->bindValue(1,$codBanner,PDO::PARAM_INT);
+            `codBanner` = $codBanner, 
+            `nombreEst` = $nombre, 
+            `institucionOrigen` = $origen, 
+            `codprograma` = $codPrograma, 
+            `programa` = $programa, 
+            `codMateria` = $codMateria, 
+            `nombreMat` = $nombreMateria, 
+            `nota` = $nota");
+            /*$save->bindValue(1,$codBanner,PDO::PARAM_INT);
             $save->bindValue(2,$nombre,PDO::PARAM_STR);
             $save->bindValue(3,$origen,PDO::PARAM_STR);
             $save->bindValue(4,$codPrograma,PDO::PARAM_STR);
             $save->bindValue(5,$programa,PDO::PARAM_STR);
             $save->bindValue(6,$codMateria,PDO::PARAM_STR);
             $save->bindValue(7,$nombreMateria,PDO::PARAM_STR);
-            $save->bindValue(8,$nota,PDO::PARAM_STR);
+            $save->bindValue(8,$nota,PDO::PARAM_STR);*/
             $save->execute();
             var_dump($save);die();
             /*return $save;

cf7caa3468b7b6208a974b886ee0f6f35fc536cb
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-16 10:05:57 -0500

    [Modificacion mafireplica][añadir historial]

--- app/models/MafiReplicaModel.php
+++ app/models/MafiReplicaModel.php
@@ -118,16 +118,15 @@ class MafiReplicaModel{
             //$numInsert= 0;
             foreach($historial as $value):
                 //var_dump($value);die();
-                $insertHistorial = $this->db->connect()->prepare("INSERT INTO `historialAcademico` SET 
-                `codBanner` = ?");
+                $insertHistorial = $this->db->connect()->prepare("INSERT INTO `historialAcademico`(`id`, `codBanner`, `nombreEst`, `institucionOrigen`, `codprograma`, `programa`, `codMateria`, `nombreMat`, `nota`) VALUES (NULL,?,?,?,?,?,?,?,?)");
                 $insertHistorial->bindValue(1,$value['bannerID'],PDO::PARAM_INT);
-                /*$insertHistorial->bindValue(2,$value['estudiante'],PDO::PARAM_STR);
+                $insertHistorial->bindValue(2,$value['estudiante'],PDO::PARAM_STR);
                 $insertHistorial->bindValue(3,'operador',PDO::PARAM_STR);
                 $insertHistorial->bindValue(4,$value['cod_programa'],PDO::PARAM_STR);
                 $insertHistorial->bindValue(5,$value['programa'],PDO::PARAM_STR);
                 $insertHistorial->bindValue(6,$value['idCurso'],PDO::PARAM_STR);
                 $insertHistorial->bindValue(7,$value['materia'],PDO::PARAM_STR);
-                $insertHistorial->bindValue(8,$value['calificacion'],PDO::PARAM_STR);*/
+                $insertHistorial->bindValue(8,$value['calificacion'],PDO::PARAM_STR);
                 $insertHistorial->execute();
                 //$numInsert++;
                 var_dump($insertHistorial);die();

c3560d762f114a055e69bd8193b6eeadf7ec476d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-17 15:23:20 -0500

    [Modificacion planeacion primer ciclo][añadir historial][excluyendo materias de moodle]

--- app/models/PlaneacionSegundoCicloModel.php
+++ app/models/PlaneacionSegundoCicloModel.php
@@ -78,6 +78,17 @@ class PlaneacionSegundoCicloModel{
         }
     }
 
+    public function materiasMoodle($codBanner){
+        try {
+            $consultaMateriaMoodle = $this->db->connect()->prepare("SELECT `Id_Banner`,`Tipo_Estudiante`,`codigomateria`,`Nota_Acumulada` FROM `datos_moodle` WHERE `Id_Banner`= ?");
+            $consultaMateriaMoodle->bindValue(1,$codBanner,PDO::PARAM_INT);
+            $consultaMateriaMoodle->execute();
+            return $consultaMateriaMoodle;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
     public function materiasPorVer($codHomologante,$programaHomologante,$materias_planeadas){
         try {
             $consultaMateriasPorVer = $this->db->connect()->prepare("SELECT mv.codBanner, mv.codMateria, mv.orden, mc.creditos, mc.ciclo,mc.prerequisito  FROM materiasPorVer mv INNER JOIN mallaCurricular mc ON mv.codMateria=mc.codigoCurso WHERE codBanner=? AND mv.codprograma = ? AND mc.codprograma = ? AND mv.codMateria NOT IN ($materias_planeadas) ORDER BY mv.orden ASC");

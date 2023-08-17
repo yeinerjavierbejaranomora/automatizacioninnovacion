e4df7b39a007ed31b1b220f1438b7e72b16f756c
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-17 11:42:29 -0500

    [Modificacion materiaspover][añadir historial][excluyendo materias de moodle]

--- app/controllers/Materiasporver.php
+++ app/controllers/Materiasporver.php
@@ -227,7 +227,6 @@ class Materiasporver extends Controller{
                 $diff = array_udiff($mallaCurricular, $historial, function($a, $b) {
                     return $a['codMateria'] <=> $b['codMateria'];
                 });
-                var_dump($diff);die();
                 if(count($historialMoodle) > 0 ):
                     var_dump($diff);die();
                     $diff = array_udiff($diff, $historialMoodle, function($a, $b) {

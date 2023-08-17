220923dabd9956bc58ec58a310b8a31504b7982d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-17 15:41:25 -0500

    [Modificacion planeacion segundo ciclo][añadir historial][excluyendo materias de moodle]

--- app/controllers/Planeacionsegundociclo.php
+++ app/controllers/Planeacionsegundociclo.php
@@ -74,7 +74,7 @@ class Planeacionsegundociclo extends Controller{
                 $materias_moodle = trim($materias_moodle, ",");
                 //var_dump($materias_planeadas, "--", $materias_moodle);die();
                 $consultaMateriasPorVer = $this->model->materiasPorVer($codHomologante,$programaHomologante,$materias_planeadas,$materias_moodle);
-                var_dump($consultaMateriasPorVer->fetchAll());die();
+                //var_dump($consultaMateriasPorVer->fetchAll());die();
                 $numeroCreditos = $this->model->getCreditosplaneados($codHomologante);
                 $numeroCreditos = $numeroCreditos->rowCount() == 0 ? 0 : $numeroCreditos->fetch(PDO::FETCH_ASSOC)['CreditosPlaneados'];
                 //var_dump($numeroCreditos);die();
@@ -234,7 +234,7 @@ class Planeacionsegundociclo extends Controller{
         endif;
         $materias_moodle = trim($materias_moodle, ",");
         $consultaMateriasPorVer = $this->model->materiasPorVer($codHomologante,$programaHomologante,$materias_planeadas,$materias_moodle);
-        var_dump($consultaMateriasPorVer);die();
+        //var_dump($consultaMateriasPorVer);die();
         $numeroCreditos = $this->model->getCreditosplaneados($codHomologante);
         $numeroCreditos = $numeroCreditos->rowCount() == 0 ? 0 : $numeroCreditos->fetch(PDO::FETCH_ASSOC)['CreditosPlaneados'];
         $numeroMateriasPorVer = $consultaMateriasPorVer->rowCount();

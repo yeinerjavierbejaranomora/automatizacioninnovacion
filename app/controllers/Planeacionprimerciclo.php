72b6e34e17e18fc3a65ba215b6b52b53f34ac091
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-25 17:21:53 -0500

    mod planeacion primer ciclo

--- app/controllers/Planeacionprimerciclo.php
+++ app/controllers/Planeacionprimerciclo.php
@@ -20,9 +20,9 @@ class Planeacionprimerciclo extends Controller{
             $marcaIngreso .= (int)$periodo['periodos'] . ",";
         }
         $marcaIngreso = trim($marcaIngreso, ",");
-        var_dump($marcaIngreso);die();
+        //var_dump($marcaIngreso);die();
         $estudiantes = $this->model->getEstudiantesNum($offset,$marcaIngreso);
-        //var_dump($estudiantes->rowCount());die();
+        var_dump($estudiantes->rowCount());die();
         $limit = 500;
         $numEstudinates = ceil($estudiantes->rowCount()/$limit);
         for ($i=0; $i < $numEstudinates; $i++) { 

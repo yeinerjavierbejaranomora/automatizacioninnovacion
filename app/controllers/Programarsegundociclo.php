ecc98fa310f0755d703c0ed7450802e4aa13ea28
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-28 15:03:34 -0500

    [Programar primer ciclo][fechas]

--- app/controllers/Programarsegundociclo.php
+++ app/controllers/Programarsegundociclo.php
@@ -54,7 +54,7 @@ class Programarsegundociclo extends Controller{
         endif;
         // $limit = 20;
         $estudiantes = $this->model->getEstudiantes($offset,$marcaIngreso,$limit);
-        var_dump($estudiantes->fetch(PDO::FETCH_ASSOC));die();
+        //var_dump($estudiantes->fetch(PDO::FETCH_ASSOC));die();
         if($estudiantes->rowCount() > 0):
             foreach ($estudiantes as $key => $estudiante) {
                 $programaHomologante = $estudiante['programa'];

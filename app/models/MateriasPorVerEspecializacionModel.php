af65b0a31358b16093cee9314fddb089c9d60898
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-29 11:36:15 -0500

    [metodo materias por ver especializacion]

--- app/models/MateriasPorVerEspecializacionModel.php
+++ app/models/MateriasPorVerEspecializacionModel.php
@@ -18,4 +18,16 @@ class MateriasPorVerEspecializacionModel {
             return false;
         }
     }
+
+    public function logAplicacion($accion,$tabla){
+        try {
+            $consultaLog = $this->db->connect()->prepare("SELECT * FROM `logAplicacion` WHERE `accion` = ? AND `tabla_afectada` = ? ORDER BY `id` DESC LIMIT 1");
+            $consultaLog->bindValue(1,$accion,PDO::PARAM_STR);
+            $consultaLog->bindValue(2,$tabla,PDO::PARAM_STR);
+            $consultaLog->execute();
+            return $consultaLog;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }

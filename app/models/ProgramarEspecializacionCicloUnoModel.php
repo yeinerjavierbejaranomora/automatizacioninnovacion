14317f099a64dc5df03310b55b77d72b1561919c
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-30 14:23:13 -0500

    [metodo programar primer ciclo ciclo  ][especializaciones]

--- app/models/ProgramarEspecializacionCicloUnoModel.php
+++ app/models/ProgramarEspecializacionCicloUnoModel.php
@@ -61,7 +61,7 @@ class ProgramarEspecializacionCicloUnoModel {
 
     public function getEstudiantes($offset,$marcaIngreso,$limit){
         try {
-            $consultaEstudiantes = $this->db->connect()->prepare("SELECT `id`,`homologante` FROM `estudiantes` 
+            $consultaEstudiantes = $this->db->connect()->prepare("SELECT `id`,`homologante`,`programa`,`bolsa`,`tipo_estudiante`,`marca_ingreso` FROM `estudiantes` 
             WHERE `id` > ?
             AND `materias_faltantes` = 'OK'
             /*AND `planeado_ciclo1` = 'OK' */

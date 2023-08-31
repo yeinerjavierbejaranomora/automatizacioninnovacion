fc0470079e47d54ea1f3ce5d8315b7d45ef1b1a8
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-31 09:51:04 -0500

    [metodo programar primer ciclo ciclo  ][especializaciones][consulta semestre]

--- app/controllers/Programarespecializacionciclouno.php
+++ app/controllers/Programarespecializacionciclouno.php
@@ -25,9 +25,9 @@ class Programarespecializacionciclouno extends Controller{
         }
         $marcaIngreso = trim($marcaIngreso, ",");
         // var_dump($marcaIngreso);
+        var_dump($marcaIngreso);die();
         $codPeriodo = substr($marcaIngreso,-2);
         $marcaIngreso = "";
-        // var_dump($marcaIngreso);die();
         
         if($fechaActual > $fechaInicioProgramacion && $fechaActual <= $fechaInicioCiclo1):
             //echo "detro de la fecha";die();

3c2923fe84dd02114e1479eaff6c09346e256375
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-31 10:58:42 -0500

    [metodo programar primer ciclo ciclo  ][especializaciones][consulta semestre]

--- app/controllers/Programarespecializacionciclouno.php
+++ app/controllers/Programarespecializacionciclouno.php
@@ -330,6 +330,7 @@ class Programarespecializacionciclouno extends Controller{
                     }
                 $consultaSemestre = $this->model->consultaSemestre($codigoBanner, $programa);
                 $semestre = $consultaSemestre->fetch(PDO::FETCH_ASSOC)['semestre'];
+                var_dump($semestre);die();
                 if(substr($marca_ingreso,-2) < $codPeriodo):
                     // echo "202343";
                     $ciclo = 2 .",". 12;

b6b9b568c308df06b2b552bcd04c7fee8792d745
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-09-01 11:50:50 -0500

    db

--- app/index.php
+++ app/index.php
@@ -2,4 +2,6 @@
 require_once 'libs/database.php';
 require_once 'libs/controller.php';
 require_once 'libs/app.php';
-var_dump(new Database()->connect());die();
+$db = new Database();
+$con = $db->connect();
+var_dump($con);die();

cd5b6e60448d966707606dc07dcf90c87c99acbc
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-09-01 11:39:39 -0500

    db

--- app/libs/database.php
+++ app/libs/database.php
@@ -3,15 +3,18 @@ class Database{
     private $host,$db,$user,$password,$charset,$pdo;
     function __construct()
     {
-        $this->host = 'localhost';
+        /*$this->host = 'localhost';
         $this->db = 'u266816196_ILPoF';
         $this->user = 'u266816196_YCF0b';
         $this->password = 'Yeiner91041755542.';
-        $this->charset = 'utf8mb4';
+        $this->charset = 'utf8mb4';*/
+        $host = '127.16.15.155';
+        $user = "VirtualIbero";
+        $password = "V1rtu4|1b3r0";
     }
 
     function connect(){
-        try {
+        /*try {
             $connection = "mysql:host=". $this->host .";dbname=". $this->db .";charset=". $this->charset;
             $options = [
                 PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
@@ -23,7 +26,31 @@ class Database{
             return $this->pdo;
         } catch (PDOException $e) {
             print_r("Error connection: ".$e->getMessage());
-        }
+        }*/
+        // Establece la conexión a MySQL
+$conexion = new mysqli($this->host, $this->user, $this->password);
+
+// Verifica si hay errores de conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// Consulta para obtener la lista de bases de datos
+$query = "SHOW DATABASES";
+$resultado = $conexion->query($query);
+
+// Verifica si la consulta fue exitosa
+if ($resultado) {
+    echo "Bases de datos disponibles:<br>";
+    while ($fila = $resultado->fetch_assoc()) {
+        echo $fila['Database'] . "<br>";
+    }
+} else {
+    echo "Error al ejecutar la consulta: " . $conexion->error;
+}
+
+// Cierra la conexión
+$conexion->close();
     }
 
     function insert_Id(){

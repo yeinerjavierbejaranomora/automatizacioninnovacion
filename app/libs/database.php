aab325e8e4e5b9b6e0ea297785f72a2fb0e041e5
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-04 08:59:54 -0500

    [prueba conexion DB]

--- app/libs/database.php
+++ app/libs/database.php
@@ -1,32 +1,31 @@
 <?php
-class Database{
-    private $host,$db,$user,$password,$charset,$pdo;
-    function __construct()
-    {
-        $this->host = 'localhost';
-        $this->db = 'u266816196_ILPoF';
-        $this->user = 'u266816196_YCF0b';
-        $this->password = 'Yeiner91041755542.';
-        $this->charset = 'utf8mb4';
-    }
+// Configura la información de la conexión
+$host = "172.16.15.155";
+$usuario = "VirtualIbero";
+$contraseña = "V1rtu4|1b3r0";
 
-    function connect(){
-        try {
-            $connection = "mysql:host=". $this->host .";dbname=". $this->db .";charset=". $this->charset;
-            $options = [
-                PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE    =>PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES      =>false,
-            ];
+// Establece la conexión a MySQL
+$conexion = new mysqli($host, $usuario, $contraseña);
 
-            $this->pdo = new PDO($connection,$this->user,$this->password,$options);
-            return $this->pdo;
-        } catch (PDOException $e) {
-            print_r("Error connection: ".$e->getMessage());
-        }
-    }
-
-    function insert_Id(){
-        return $this->pdo->lastInsertId();
-    }
+// Verifica si hay errores de conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
 }
+
+// Consulta para obtener la lista de bases de datos
+$query = "SHOW DATABASES";
+$resultado = $conexion->query($query);
+
+// Verifica si la consulta fue exitosa
+if ($resultado) {
+    echo "Bases de datos disponibles:<br>";
+    while ($fila = $resultado->fetch_assoc()) {
+        echo $fila['Database'] . "<br>";
+    }
+} else {
+    echo "Error al ejecutar la consulta: " . $conexion->error;
+}
+
+// Cierra la conexión
+$conexion->close();
+?>
---
+++ app/libs/database2.php
@@ -0,0 +1,12 @@
+<?php
+class Database2 {
+    private $host,$db,$user,$password,$charset,$pdo;
+    function __construct()
+    {
+        $this->host = '172.16.15.155';
+        // $this->db = 'u266816196_ILPoF';
+        $this->user = 'u266816196_YCF0b';
+        $this->password = 'Yeiner91041755542.';
+        $this->charset = 'utf8mb4';
+    }
+}

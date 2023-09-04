dfd6e9c70a190124c439123626f396a956050636
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-04 09:51:20 -0500

    [metodo programar ][especializaciones][modificacion mensaje alerta]

--- app/index.php
+++ app/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'libs/database.php';
-// require_once 'libs/database2.php';
+// require_once 'libs/database.php';
+require_once 'libs/database2.php';
 require_once 'libs/controller.php';
 require_once 'libs/app.php';
-$db = new Database();
+$db = new Database2();
 $con = $db->connect();
 //var_dump($con);die();

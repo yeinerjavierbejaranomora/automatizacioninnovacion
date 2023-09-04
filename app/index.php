683635c974a57a7a73d3134d916187c0385e2e3a
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-04 09:22:24 -0500

    [prueba conexion DB]

--- app/index.php
+++ app/index.php
@@ -3,6 +3,31 @@
 require_once 'libs/database2.php';
 require_once 'libs/controller.php';
 require_once 'libs/app.php';
-$db = new Database2();
-$con = $db->connect();
-//var_dump($con);die();
+$host = "172.16.15.155";
+$usuario = "VirtualIbero";
+$contraseña = "V1rtu4|1b3r0";
+
+// Establece la conexión a MySQL
+$conexion = new mysqli($host, $usuario, $contraseña);
+
+// Verifica si hay errores de conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// Consulta para obtener la lista de bases de datos
+$query = "SHOW DATABASES";
+$resultado = $conexion->query($query);
+
+// Verifica si la consulta fue exitosa
+if ($resultado) {
+    echo "Bases de datos disponibles:<br>";
+    while ($fila = $resultado->fetch_assoc()) {
+        echo $fila['Database'] . "<br>";
+    }
+} else {
+    echo "Error al ejecutar la consulta: " . $conexion->error;
+}
+
+// Cierra la conexión
+$conexion->close();

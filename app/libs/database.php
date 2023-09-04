42a9fb503dc08482c0143a91c2d171d79159d1e2
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-04 09:44:28 -0500

    [metodo programar ][especializaciones][modificacion mensaje alerta]

--- app/libs/database.php
+++ app/libs/database.php
@@ -3,15 +3,18 @@ class Database{
     private $host,$db,$user,$password,$charset,$pdo;
     function __construct()
     {
-        $this->host = 'localhost';
+        /*$this->host = 'localhost';
         $this->db = 'u266816196_ILPoF';
         $this->user = 'u266816196_YCF0b';
         $this->password = 'Yeiner91041755542.';
-        $this->charset = 'utf8mb4';
+        $this->charset = 'utf8mb4';*/
+        $this->host = "127.0.0.1";
+        $this->user = "u266816196_YCF0b";
+        $this->password = "Yeiner91041755542.";   
     }
 
     function connect(){
-        try {
+        /*try {
             $connection = "mysql:host=". $this->host .";dbname=". $this->db .";charset=". $this->charset;
             $options = [
                 PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
@@ -23,6 +26,28 @@ class Database{
             return $this->pdo;
         } catch (PDOException $e) {
             print_r("Error connection: ".$e->getMessage());
+        }*/
+        try {
+        
+            // Crea una instancia de PDO
+            $dsn = "mysql:host=".$this->host .";port=3306";
+            $conexion = new PDO($dsn, $this->user, $this->password);
+        
+            // Configura PDO para lanzar excepciones en errores
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            // Ejecuta una consulta para obtener la lista de bases de datos
+            $consulta = $conexion->query("SHOW DATABASES");
+        
+            // Obtiene los resultados en un array
+            $basesDeDatos = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        
+            // Itera a través de la lista de bases de datos
+            foreach ($basesDeDatos as $fila) {
+                echo $fila['Database'] . "<br>";
+            }
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
         }
     }
 

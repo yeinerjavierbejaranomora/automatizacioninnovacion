6a6047bcdd2b547be7feb6894e189a0108a855dd
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-13 14:45:34 -0500

    [prueba conexion DB]

--- app/index.php
+++ app/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'libs/database.php';
-//require_once 'libs/database2.php';
+//require_once 'libs/database.php';
+require_once 'libs/database2.php';
 require_once 'libs/controller.php';
 require_once 'libs/app.php';
-phpinfo();die();
-/*$db = new Database2();
-$con = $db->connect();*/
+//phpinfo();die();
+$db = new Database2();
+$con = $db->connect();
 //var_dump($con);die();
